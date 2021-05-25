70d55ba1942ce2d04201fccb0cfe823c84c76bba
Vasu Bansal <84651595+spooderman-x@users.noreply.github.com>
2021-05-25 19:58:38 +0530

    Update rectangle.cpp

--- Stars/rectangle.cpp
+++ Stars/rectangle.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    cout << "No. of rows: \n";
+    int row;
+    cin >> row;
+    cout << "No.of cols: \n";
+    int cols;
+    cin >> cols;
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<cols;j++)
+            cout << "* " ;
+            
+        cout << "\n"; 
+            
+    }
+    
+    return 0;
+}
